9a3e36ee25781a8497a7e3789fd631fdb8532d48
Felipe Vieira Aburaya <felipe.aburaya@hotmail.com>
2021-08-22 23:07:52 +0000

    Richtet CI mit Azure Pipelines ein [skip ci]

--- Reusable.DataAccess.Cosmos.IntegrationTests/CosmosDatabaseFixture.cs
+++ Reusable.DataAccess.Cosmos.IntegrationTests/CosmosDatabaseFixture.cs
@@ -8,7 +8,7 @@ namespace Reusable.DataAccess.Cosmos.IntegrationTests
     public class CosmosDatabaseFixture : IDisposable
     {
         private string ConnectionString =>
-            "AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+            $"AccountEndpoint={Environment.GetEnvironmentVariable("CosmosDbEmulator.Endpoint") ?? "https://localhost:8081/"};AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
 
         private string DatabaseName => "Reusable.DataAccess.IntegrationTests";
 
