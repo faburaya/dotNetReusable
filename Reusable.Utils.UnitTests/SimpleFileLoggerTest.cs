5f66a4dfd766a17715f57fe3e60c39df36e9715a
Felipe Aburaya <felipe.aburaya@hotmail.com>
2021-11-16 21:54:35 +0100

    Führt neue einfache Protokollierung ein, die auf eine Datei schreibt.

---
+++ Reusable.Utils.UnitTests/SimpleFileLoggerTest.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace Reusable.Utils.UnitTests
+{
+    public class SimpleFileLoggerTest
+    {
+        [Fact]
+        public void InstantiateAndDispose_FileDoesNotExist()
+        {
+            const string filePath = "log.txt";
+            var log = new SimpleFileLogger(filePath);
+            log.Dispose();
+            File.Delete(filePath);
+        }
+
+        [Fact]
+        public void InstantiateAndDispose_FileExists()
+        {
+            const string filePath = "log.txt";
+            File.WriteAllText(filePath, "Etwas.");
+            var log = new SimpleFileLogger(filePath);
+            log.Dispose();
+            File.Delete(filePath);
+        }
+
+        [Fact]
+        public void LogOneMessage_SingleThread()
+        {
+            const string message = "Nachricht";
+            const string filePath = "log.txt";
+            using (var log = new SimpleFileLogger(filePath))
+            {
+                log.Trace(message);
+            }
+
+            string actualContent = File.ReadAllText(filePath);
+            Assert.Contains(message, actualContent);
+            File.Delete(filePath);
+        }
+
+        private delegate bool FindSubstring(string substring, string text);
+
+        private static void AssertThatEverySubstringIsContainedInDistinctLine(
+            IEnumerable<string> substrings, IList<string> lines, FindSubstring predicate)
+        {
+            foreach (string substring in substrings)
+            {
+                int idx;
+                for (idx = 0; idx < lines.Count; ++idx)
+                {
+                    if (lines[idx] == null || !predicate(substring, lines[idx]))
+                    {
+                        continue;
+                    }
+
+                    lines[idx] = null;
+                    break;
+                }
+
+                Assert.True(idx != lines.Count, $"'{substring}' kann nicht gefunden werden!");
+            }
+        }
+
+        [Fact]
+        public void LogManyMessages_SingleThread()
+        {
+            string[] messages = With<string>.CreateArrayOf(100, (int index) => $"Nachricht #{index}");
+
+            const string filePath = "log.txt";
+            using (var log = new SimpleFileLogger(filePath))
+            {
+                foreach (string message in messages)
+                {
+                    log.Trace(message);
+                }
+            }
+
+            string[] actualLines = File.ReadAllLines(filePath);
+            Assert.Equal(messages.Count(), actualLines.Count());
+            AssertThatEverySubstringIsContainedInDistinctLine(messages, actualLines,
+                (message, line) => line.EndsWith(message));
+
+            File.Delete(filePath);
+        }
+
+        [Fact]
+        public void LogMessagesFromAllLevels_SingleThread()
+        {
+            const string filePath = "log.txt";
+            using (var log = new SimpleFileLogger(filePath))
+            {
+                string message = "Nachricht";
+                log.Trace(message);
+                log.Debug(message);
+                log.Info(message);
+                log.Warning(message);
+                log.Error(message);
+                log.Critical(message);
+            }
+
+            string[] actualLines =
+                (from line in File.ReadAllLines(filePath) select line.ToLower()).ToArray();
+
+            var expectedLabels = new[] { "trace", "debug", "info", "warning", "error", "critical" };
+
+            Assert.Equal(expectedLabels.Count(), actualLines.Count());
+            AssertThatEverySubstringIsContainedInDistinctLine(expectedLabels, actualLines,
+                (message, line) => line.Contains(message));
+
+            File.Delete(filePath);
+        }
+
+        [Fact]
+        public void LogManyMessages_Concurrently()
+        {
+            const int listSize = 1000;
+            string[][] messageLists = new[] {
+                With<string>.CreateArrayOf(listSize, (int index) => $"Nachricht #{index}"),
+                With<string>.CreateArrayOf(listSize, (int index) => $"Nachricht #{index + listSize}"),
+                With<string>.CreateArrayOf(listSize, (int index) => $"Nachricht #{index + 2*listSize}"),
+            };
+
+            static void LogAllMessages(ILogger log, string[] messages)
+            {
+                foreach (string message in messages)
+                {
+                    log.Trace(message);
+                }
+            }
+
+            const string filePath = "log.txt";
+            using (var log = new SimpleFileLogger(filePath))
+            {
+                Task.WaitAll((
+                    from list in messageLists
+                    select Task.Run(() => LogAllMessages(log, list))
+                ).ToArray());
+            }
+
+            string[] actualLines = File.ReadAllLines(filePath);
+            Assert.Equal(messageLists.Count() * listSize, actualLines.Count());
+
+            AssertThatEverySubstringIsContainedInDistinctLine(
+                With<string>.ConcatenateViewOf(messageLists),
+                actualLines,
+                (message, line) => line.EndsWith(message));
+
+            File.Delete(filePath);
+        }
+    }
+}
---
+++ Reusable.Utils/SimpleFileLogger.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Reusable.Utils
+{
+    /// <summary>
+    /// Bietet einfache Protokollierung, die auf eine Datei geschrieben wird.
+    /// </summary>
+    public class SimpleFileLogger : SimpleLoggerBase, IDisposable
+    {
+        private readonly FileStream _fileStream;
+
+        private readonly StreamWriter _streamWriter;
+
+        private readonly ConcurrentQueue<string> _eventsQueue;
+
+        private readonly Task _recorderTask;
+
+        /// <summary>
+        /// Erstellt eine neue Instanz von <see cref="SimpleFileLogger"/>.
+        /// </summary>
+        /// <param name="logFilePath">Das Pfad der Datei, auf die protokolliert wird.</param>
+        public SimpleFileLogger(string logFilePath)
+        {
+            _disposed = false;
+            _fileStream = new FileStream(
+                logFilePath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.Read);
+            _streamWriter = new StreamWriter(_fileStream);
+            _eventsQueue = new ConcurrentQueue<string>();
+            _recorderTask = Task.Run(RecordEventsAsynchronously);
+        }
+
+        /// <inheritdoc/>
+        protected override void WriteLine(string text)
+        {
+            _eventsQueue.Enqueue(text);
+        }
+
+        private void RecordEventsAsynchronously()
+        {
+            while (true)
+            {
+                while (_eventsQueue.TryDequeue(out string text))
+                {
+                    if (text == null)
+                        return; // muss das Thread beenden
+
+                    _streamWriter.WriteLine(text);
+                }
+
+                Thread.Sleep(50);
+            }
+        }
+
+        private bool _disposed;
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            if (_disposed)
+                return;
+
+            // fordert das Ende des Threads und wartet darauf
+            _eventsQueue.Enqueue(null);
+            _recorderTask.Wait();
+            _recorderTask.Dispose();
+
+            _streamWriter.Dispose();
+            _fileStream.Dispose();
+
+            _disposed = true;
+            GC.SuppressFinalize(this);
+        }
+    }
+}
