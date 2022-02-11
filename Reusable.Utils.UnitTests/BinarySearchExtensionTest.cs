e7ca469145f1551c1c990232e618a93cf71f6956
Felipe Aburaya <felipe.aburaya@hotmail.com>
2022-02-11 21:05:22 +0100

    Führt nützliche Algorithmen für eine binäre Suche ein.

---
+++ Reusable.Utils.UnitTests/BinarySearchExtensionTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xunit;
+using Reusable.Utils;
+
+namespace Reusable.Utils.Tests
+{
+    public class BinarySearchExtensionTest
+    {
+        [Fact]
+        public void SearchLowerBoundIndex_WhenListEmpty_ThenZero()
+        {
+            var emptyList = new int[0];
+            Assert.Equal(0, emptyList.SearchLowerBoundIndex(key: 1, x => x));
+        }
+
+        [Fact]
+        public void SearchLowerBoundIndex_WhenKeyTooLow_ThenGiveZero()
+        {
+            var list = new int[] { 1, 2, 3, 4, 5 };
+            Assert.Equal(0, list.SearchLowerBoundIndex(key: 0, x => x));
+        }
+
+        [Fact]
+        public void SearchLowerBoundIndex_WhenKeyTooHigh_ThenGiveCount()
+        {
+            var list = new int[] { 1, 2, 3, 4, 5 };
+            Assert.Equal(list.Length, list.SearchLowerBoundIndex(key: 9, x => x));
+        }
+
+        [Fact]
+        public void SearchLowerBoundIndex_WhenKeyPresent_ThenGiveItsIndex()
+        {
+            var list = new int[] { 1, 2, 3, 4, 5 };
+            Assert.Equal(0, list.SearchLowerBoundIndex(key: 1, x => x));
+            Assert.Equal(2, list.SearchLowerBoundIndex(key: 3, x => x));
+            Assert.Equal(4, list.SearchLowerBoundIndex(key: 5, x => x));
+        }
+
+        [Fact]
+        public void SearchLowerBoundIndex_WhenKeyRepeats_ThenGiveIndexFirstOccurrence()
+        {
+            var list = new int[] { 1, 2, 3, 3, 3, 4, 5 };
+            Assert.Equal(2, list.SearchLowerBoundIndex(key: 3, x => x));
+        }
+        [Fact]
+        public void SearchUpperBoundIndex_WhenListEmpty_ThenZero()
+        {
+            var emptyList = new int[0];
+            Assert.Equal(0, emptyList.SearchUpperBoundIndex(key: 1, x => x));
+        }
+
+        [Fact]
+        public void SearchUpperBoundIndex_WhenKeyTooLow_ThenGiveZero()
+        {
+            var list = new int[] { 1, 2, 3, 4, 5 };
+            Assert.Equal(0, list.SearchUpperBoundIndex(key: 0, x => x));
+        }
+
+        [Fact]
+        public void SearchUpperBoundIndex_WhenKeyTooHigh_ThenGiveCount()
+        {
+            var list = new int[] { 1, 2, 3, 4, 5 };
+            Assert.Equal(list.Length, list.SearchUpperBoundIndex(key: 9, x => x));
+        }
+
+        [Fact]
+        public void SearchUpperBoundIndex_WhenKeyPresent_ThenGiveNextIndex()
+        {
+            var list = new int[] { 1, 2, 3, 4, 5 };
+            Assert.Equal(1, list.SearchUpperBoundIndex(key: 1, x => x));
+            Assert.Equal(3, list.SearchUpperBoundIndex(key: 3, x => x));
+            Assert.Equal(5, list.SearchUpperBoundIndex(key: 5, x => x));
+        }
+
+        [Fact]
+        public void SearchUpperBoundIndex_WhenKeyRepeats_ThenGiveIndexPastLastOccurrence()
+        {
+            var list = new int[] { 1, 2, 3, 3, 3, 3, 4, 5 };
+            Assert.Equal(6, list.SearchUpperBoundIndex(key: 3, x => x));
+        }
+    }
+}
---
+++ Reusable.Utils/BinarySearchExtension.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Reusable.Utils
+{
+    /// <summary>
+    /// Implementiert eine allgemeine binäre Suche für sortierte Listen.
+    /// </summary>
+    public static class BinarySearchExtension
+    {
+        /// <summary>
+        /// Findet in der Liste die untere Grenze ("lower bound"):
+        /// Das erste Element, das den gegebenen Schlüssel aufweist, wenn es vorhaden ist,
+        /// andernfalls ist es das unmittelbar nächste Element.
+        /// </summary>
+        /// <typeparam name="KeyType">Der Typ des Schlüssels.</typeparam>
+        /// <typeparam name="DataType">Der Datentyp.</typeparam>
+        /// <param name="values">Die zu suchende Liste.</param>
+        /// <param name="key">Der Schlüssel des erwünschten Elements.</param>
+        /// <param name="getKeyOf">Gibt den Schlüssel eines gegebenen Elements zurück.</param>
+        /// <returns>Der Index der unteren Grenze.</returns>
+        public static int SearchLowerBoundIndex<KeyType, DataType>(this IList<DataType> values,
+                                                                   KeyType key,
+                                                                   Func<DataType, KeyType> getKeyOf)
+            where KeyType : IComparable<KeyType>
+        {
+            int low = 0;
+            int high = values.Count;
+            int found = BinarySearchImpl(key, values, ref low, ref high, getKeyOf);
+
+            while (low != high)
+            {
+                high = found;
+                found = BinarySearchImpl(key, values, ref low, ref high, getKeyOf);
+            }
+
+            return found;
+        }
+
+        /// <summary>
+        /// Findet in der Liste die obere Grenze ("upper bound"):
+        /// Das erste Element, das einen Schlüssel aufweist, das größer als der gegebener Schlüssel ist.
+        /// </summary>
+        /// <typeparam name="KeyType">Der Typ des Schlüssels.</typeparam>
+        /// <typeparam name="DataType">Der Datentyp.</typeparam>
+        /// <param name="values">Die zu suchende Liste.</param>
+        /// <param name="key">Der Schlüssel des erwünschten Elements.</param>
+        /// <param name="getKeyOf">Gibt den Schlüssel eines gegebenen Elements zurück.</param>
+        /// <returns>Der Index der oberen Grenze.</returns>
+        public static int SearchUpperBoundIndex<KeyType, DataType>(this IList<DataType> values,
+                                                                   KeyType key,
+                                                                   Func<DataType, KeyType> getKeyOf)
+            where KeyType : IComparable<KeyType>
+        {
+            int low = 0;
+            int high = values.Count;
+            int found = BinarySearchImpl(key, values, ref low, ref high, getKeyOf);
+
+            while (low != high)
+            {
+                low = found + 1;
+                found = BinarySearchImpl(key, values, ref low, ref high, getKeyOf);
+            }
+
+            return found;
+        }
+
+        private static int BinarySearchImpl<KeyType, DataType>(KeyType key,
+                                                               IList<DataType> values,
+                                                               ref int low,
+                                                               ref int high,
+                                                               Func<DataType, KeyType> getKeyOf)
+            where KeyType : IComparable<KeyType>
+        {
+            Debug.Assert(low <= high);
+
+            while (low < high)
+            {
+                int middle = (low + high) / 2;
+
+                if (getKeyOf(values[middle]).CompareTo(key) < 0)
+                {
+                    low = middle + 1;
+                }
+                else if (getKeyOf(values[middle]).CompareTo(key) > 0)
+                {
+                    high = middle;
+                }
+                else
+                {
+                    return middle;
+                }
+            }
+
+            return high;
+        }
+    }
+}
