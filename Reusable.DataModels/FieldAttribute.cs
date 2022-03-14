ef6ac8a07bcec17e8df892f40ffc6022fc97b5fd
Felipe Vieira Aburaya <felipe.aburaya@hotmail.com>
2022-03-14 21:20:27 +0100

    Führt neues FieldAttribut ein.

---
+++ Reusable.DataModels/FieldAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Reusable.DataModels
+{
+    /// <summary>
+    /// Dieses Attribut beschreibt das Feld, mit dem ein Property in Verbindung steht.
+    /// Dieses Feld darf zu einem Formular, zu einer Tabelle einer Datenbank oder Ähnliches gehören.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class FieldAttribute : Attribute
+    {
+        /// <summary>
+        /// Der Name des Felds.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
