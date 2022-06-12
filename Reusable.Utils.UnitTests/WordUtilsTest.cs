7c14b41b6d968071837c9da035be764688927496
Felipe Vieira Aburaya <felipe.aburaya@hotmail.com>
2022-06-12 23:08:50 +0200

    Führt Algorithmen für die Handhabung von Wörtern in einer Zeichenkette ein.

---
+++ Reusable.Utils.UnitTests/WordUtilsTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xunit;
+
+namespace Reusable.Utils.UnitTests
+{
+    public class WordUtilsTest
+    {
+        [Fact]
+        public void FindEndOfWord_WhenWithinWord_ThenFindEnd()
+        {
+            const string text = "Eine kurze Geschichte: Ende";
+            Assert.Equal(10, WordUtils.FindEndOfWord(text, 7));
+            Assert.Equal(21, WordUtils.FindEndOfWord(text, 15));
+            Assert.Equal(text.Length, WordUtils.FindEndOfWord(text, 24));
+        }
+
+        [Fact]
+        public void FindEndOfWord_WhenOutOfWord_ThenReturnSameIndex()
+        {
+            const string text = "Eine kurze Geschichte: Ende";
+            Assert.Equal(4, WordUtils.FindEndOfWord(text, 4));
+            Assert.Equal(21, WordUtils.FindEndOfWord(text, 21));
+            Assert.Equal(22, WordUtils.FindEndOfWord(text, 22));
+        }
+
+        [Fact]
+        public void FindStartOfWord_WhenWithinWord_ThenFindStart()
+        {
+            const string text = "Arbeiter&Bourgeoisie kämpfen.";
+            Assert.Equal(0, WordUtils.FindStartOfWord(text, 3));
+            Assert.Equal(9, WordUtils.FindStartOfWord(text, 12));
+            Assert.Equal(21, WordUtils.FindStartOfWord(text, 24));
+        }
+
+        [Fact]
+        public void FindStartOfWord_WhenOutOfWord_ThenReturnSameIndex()
+        {
+            const string text = "Hallo! Tchüss.";
+            Assert.Equal(5, WordUtils.FindStartOfWord(text, 5));
+            Assert.Equal(6, WordUtils.FindStartOfWord(text, 6));
+            Assert.Equal(13, WordUtils.FindStartOfWord(text, 13));
+        }
+
+        [Fact]
+        public void FindClosestStartOrEndOfWord_WhenWithinWord_ThenFindStartOrEnd()
+        {
+            const string text = "abcd, abcd";
+            Assert.Equal(0, WordUtils.FindClosestStartOrEndOfWord(text, 1));
+            Assert.Equal(4, WordUtils.FindClosestStartOrEndOfWord(text, 2));
+            Assert.Equal(6, WordUtils.FindClosestStartOrEndOfWord(text, 7));
+            Assert.Equal(10, WordUtils.FindClosestStartOrEndOfWord(text, 8));
+        }
+
+        [Fact]
+        public void FindClosestStartOrEndOfWord_WhenOutOfWord_ThenReturnSameIndex()
+        {
+            const string text = "abcd, abcd";
+            Assert.Equal(4, WordUtils.FindClosestStartOrEndOfWord(text, 4));
+            Assert.Equal(5, WordUtils.FindClosestStartOrEndOfWord(text, 5));
+        }
+
+        [Fact]
+        public void SplitIntoWords_WhenRange_ThenGiveWordsInRange()
+        {
+            const string text = "Noch mal: nichts Neues";
+            Assert.Equal(new[] { "mal", "nichts", "Neues" }, WordUtils.SplitIntoWords(text, 4, text.Length));
+            Assert.Equal(new[] { "Noch", "mal", "nichts" }, WordUtils.SplitIntoWords(text, 0, 16));
+            Assert.Equal(new[] { "mal", "nichts" }, WordUtils.SplitIntoWords(text, 4, 16));
+        }
+
+        [Fact]
+        public void SplitIntoWordsAsRefs_WhenRange_ThenGiveWordsInRange()
+        {
+            const string text = "Noch mal: nichts Neues";
+
+            SubstringRef[] refs = new[] {
+                new SubstringRef(text, 0, 4),
+                new SubstringRef(text, 5, 3),
+                new SubstringRef(text, 10, 6),
+                new SubstringRef(text, 17, 5),
+            };
+            Assert.Equal(refs.Skip(1).Take(3), WordUtils.SplitIntoWordsAsRefs(text, 4, text.Length));
+            Assert.Equal(refs.Take(3), WordUtils.SplitIntoWordsAsRefs(text, 0, 16));
+            Assert.Equal(refs.Skip(1).Take(2), WordUtils.SplitIntoWordsAsRefs(text, 4, 16));
+        }
+    }
+}
---
+++ Reusable.Utils/WordUtils.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Reusable.Utils
+{
+    /// <summary>
+    /// Sammlung von Algorithmen, die bei der Arbeit mit Wörtern nützlich sind.
+    /// </summary>
+    public static class WordUtils
+    {
+        private static readonly Regex _notWordForwardRegex = new(@"([^\w]+)", RegexOptions.Compiled);
+
+        /// <summary>
+        /// Findet das Ende eines Wortes.
+        /// </summary>
+        /// <param name="text">Der Text.</param>
+        /// <param name="start">Ab dieser Indexposition nach rechts startet die Suche.</param>
+        /// <returns>Falls <paramref name="start"/> mitten in einem Wort liegt, dann gibt die Indexposition nach seinem letzten Charakter zurück, sonst <paramref name="start"/>.</returns>
+        public static int FindEndOfWord(string text, int start)
+        {
+            Match nextMatch = _notWordForwardRegex.Match(text, start);
+            return (nextMatch.Success ? nextMatch.Index : text.Length);
+        }
+
+        private static readonly Regex _notWordBackwardRegex =
+            new(@"([^\w]+)", RegexOptions.Compiled | RegexOptions.RightToLeft);
+
+        /// <summary>
+        /// Findet den Anfang eines Wortes.
+        /// </summary>
+        /// <param name="text">Der Text.</param>
+        /// <param name="last">Ab dieser Indexposition nach links startet die Suche.</param>
+        /// <returns>Falls <paramref name="last"/> mitten in einem Wort liegt, dann gibt die Indexposition seines ersten Charakters zurück, sonst <paramref name="last"/>.</returns>
+        public static int FindStartOfWord(string text, int last)
+        {
+            Match previousMatch = _notWordBackwardRegex.Match(text, last + 1);
+            return Math.Min(last, previousMatch.Success ? previousMatch.Index + previousMatch.Length : 0);
+        }
+
+        /// <summary>
+        /// Findet den Anfang oder das Ende eines Wortes, je nachdem, welche näher ist.
+        /// </summary>
+        /// <param name="text">Der Text.</param>
+        /// <param name="pos">Es wird um diese Indexposition rückwärts nach dem Anfang des Wortes und vorwärts nach dem Ende des Wortes gesucht.</param>
+        /// <returns>Falls <paramref name="pos"/> innerhalb eines Wort liegt, dann gibt die Indexposition des Anfangs oder des Endes des Wortes zurück, je nachdem, welche näher ist. Sonst, gibt <paramref name="pos"/> zurück.</returns>
+        public static int FindClosestStartOrEndOfWord(string text, int pos)
+        {
+            int previous = FindStartOfWord(text, pos);
+            int next = FindEndOfWord(text, pos);
+
+            if (next - pos <= pos - previous)
+            {
+                return next;
+            }
+            else
+            {
+                return previous;
+            }
+        }
+
+        private static readonly Regex _wordRegex = new(@"([\w]+)");
+
+        private static List<OutType> SplitIntoWordsImpl<OutType>(
+            string text, int begin, int end, Func<Match, OutType> callback)
+        {
+            List<OutType> words = new();
+            foreach (Match match in _wordRegex.Matches(text, begin))
+            {
+                if (match.Index + match.Length <= end)
+                {
+                    words.Add(callback(match));
+                }
+                else
+                    break;
+            }
+            return words;
+        }
+
+        /// <summary>
+        /// Spaltet einen Bereich im Text in Wörter auf.
+        /// </summary>
+        /// <param name="text">Der Text.</param>
+        /// <param name="begin">Legt den Bereich fest: die Indexposition des ersten Charakter.</param>
+        /// <param name="end">Legt den Bereich fest: die Indexposition nach dem letzten Charakter.</param>
+        /// <returns>Eine Liste mit Instanzen von <see cref="SubstringRef"/>, die auf die Wörter verweisen.</returns>
+        public static List<SubstringRef> SplitIntoWordsAsRefs(string text, int begin, int end)
+        {
+            return SplitIntoWordsImpl(text, begin, end,
+                (Match match) => new SubstringRef(match, text));
+        }
+
+        /// <summary>
+        /// Spaltet einen Bereich im Text in Wörter auf.
+        /// </summary>
+        /// <param name="text">Der Text.</param>
+        /// <param name="begin">Legt den Bereich fest: die Indexposition des ersten Charakter.</param>
+        /// <param name="end">Legt den Bereich fest: die Indexposition nach dem letzten Charakter.</param>
+        /// <returns>Eine Liste mit Kopien der aufgespalteten Wörter.</returns>
+        public static List<string> SplitIntoWords(string text, int begin, int end)
+        {
+            return SplitIntoWordsImpl(text, begin, end,
+                (Match match) => match.Value);
+        }
+    }
+}
