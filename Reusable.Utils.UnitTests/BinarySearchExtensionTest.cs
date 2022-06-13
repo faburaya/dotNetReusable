f98ed80ab751df01157bd14b27a6d7a8bea3f9f7
Felipe Vieira Aburaya <felipe.aburaya@hotmail.com>
2022-06-14 01:58:52 +0200

    Fügt Überladung der Suche nach unterer und oberer Grenze hinzu.

--- Reusable.Utils.UnitTests/BinarySearchExtensionTest.cs
+++ Reusable.Utils.UnitTests/BinarySearchExtensionTest.cs
@@ -1,4 +1,6 @@
-﻿using Xunit;
+﻿using System;
+
+using Xunit;
 
 namespace Reusable.Utils.UnitTests
 {
@@ -40,6 +42,16 @@ namespace Reusable.Utils.UnitTests
             var list = new int[] { 1, 2, 3, 3, 3, 4, 5 };
             Assert.Equal(2, list.SearchLowerBoundIndex(key: 3, x => x));
         }
+
+        [Fact]
+        public void SearchLowerBoundIndex_WhenDescendingOrder_ThenLowerBoundStillHasLowerIndex()
+        {
+            static int compare(int a, int b) => -1 * a.CompareTo(b);
+            var list = new int[] { 5, 4, 3, 3, 3, 2, 1 };
+            Assert.Equal(2, list.SearchLowerBoundIndex(key: 3, x => x, compare));
+            Assert.Equal(5, list.SearchLowerBoundIndex(key: 2, x => x, compare));
+        }
+
         [Fact]
         public void SearchUpperBoundIndex_WhenListEmpty_ThenZero()
         {
@@ -75,6 +87,15 @@ namespace Reusable.Utils.UnitTests
         {
             var list = new int[] { 1, 2, 3, 3, 3, 3, 4, 5 };
             Assert.Equal(6, list.SearchUpperBoundIndex(key: 3, x => x));
+        }
+
+        [Fact]
+        public void SearchUpperBoundIndex_WhenDescendingOrder_ThenUpperBoundStillHasHigherIndex()
+        {
+            static int compare(int a, int b) => -1 * a.CompareTo(b);
+            var list = new int[] { 5, 4, 3, 3, 3, 3, 2, 1 };
+            Assert.Equal(6, list.SearchUpperBoundIndex(key: 3, x => x, compare));
+            Assert.Equal(7, list.SearchUpperBoundIndex(key: 2, x => x, compare));
         }
     }
 }
--- Reusable.Utils/BinarySearchExtension.cs
+++ Reusable.Utils/BinarySearchExtension.cs
@@ -19,20 +19,70 @@ namespace Reusable.Utils
         /// <param name="values">Die zu suchende Liste.</param>
         /// <param name="key">Der Schlüssel des erwünschten Elements.</param>
         /// <param name="getKeyOf">Gibt den Schlüssel eines gegebenen Elements zurück.</param>
+        /// <param name="compare">Diese Rückrufaktion legt fest, wie die Schlüssel miteinander zu vergleichen und zu ordnen sind.</param>
+        /// <returns>Der Index der unteren Grenze.</returns>
+        public static int SearchLowerBoundIndex<KeyType, DataType>(this IList<DataType> values,
+                                                                   KeyType key,
+                                                                   Func<DataType, KeyType> getKeyOf,
+                                                                   Comparison<KeyType> compare)
+        {
+            int low = 0;
+            int high = values.Count;
+            int found = BinarySearchImpl(key, values, ref low, ref high, getKeyOf, compare);
+
+            while (low != high)
+            {
+                high = found;
+                found = BinarySearchImpl(key, values, ref low, ref high, getKeyOf, compare);
+            }
+
+            return found;
+        }
+
+        /// <summary>
+        /// Findet in der Liste die untere Grenze ("lower bound"):
+        /// Das erste Element, das den gegebenen Schlüssel aufweist, wenn es vorhaden ist,
+        /// andernfalls ist es das unmittelbar nächste Element.
+        /// </summary>
+        /// <typeparam name="KeyType">Der Typ des Schlüssels.</typeparam>
+        /// <typeparam name="DataType">Der Datentyp.</typeparam>
+        /// <param name="values">Die zu suchende Liste.</param>
+        /// <param name="key">Der Schlüssel des erwünschten Elements.</param>
+        /// <param name="getKeyOf">Gibt den Schlüssel eines gegebenen Elements zurück.</param>
         /// <returns>Der Index der unteren Grenze.</returns>
         public static int SearchLowerBoundIndex<KeyType, DataType>(this IList<DataType> values,
                                                                    KeyType key,
                                                                    Func<DataType, KeyType> getKeyOf)
             where KeyType : IComparable<KeyType>
         {
+            return SearchLowerBoundIndex(values, key, getKeyOf,
+                (KeyType a, KeyType b) => a.CompareTo(b));
+        }
+
+        /// <summary>
+        /// Findet in der Liste die obere Grenze ("upper bound"):
+        /// Das erste Element, das einen Schlüssel aufweist, das größer als der gegebener Schlüssel ist.
+        /// </summary>
+        /// <typeparam name="KeyType">Der Typ des Schlüssels.</typeparam>
+        /// <typeparam name="DataType">Der Datentyp.</typeparam>
+        /// <param name="values">Die zu suchende Liste.</param>
+        /// <param name="key">Der Schlüssel des erwünschten Elements.</param>
+        /// <param name="getKeyOf">Gibt den Schlüssel eines gegebenen Elements zurück.</param>
+        /// <param name="compare">Diese Rückrufaktion legt fest, wie die Schlüssel miteinander zu vergleichen und zu ordnen sind.</param>
+        /// <returns>Der Index der oberen Grenze.</returns>
+        public static int SearchUpperBoundIndex<KeyType, DataType>(this IList<DataType> values,
+                                                                   KeyType key,
+                                                                   Func<DataType, KeyType> getKeyOf,
+                                                                   Comparison<KeyType> compare)
+        {
             int low = 0;
             int high = values.Count;
-            int found = BinarySearchImpl(key, values, ref low, ref high, getKeyOf);
+            int found = BinarySearchImpl(key, values, ref low, ref high, getKeyOf, compare);
 
             while (low != high)
             {
-                high = found;
-                found = BinarySearchImpl(key, values, ref low, ref high, getKeyOf);
+                low = found + 1;
+                found = BinarySearchImpl(key, values, ref low, ref high, getKeyOf, compare);
             }
 
             return found;
@@ -53,25 +103,16 @@ namespace Reusable.Utils
                                                                    Func<DataType, KeyType> getKeyOf)
             where KeyType : IComparable<KeyType>
         {
-            int low = 0;
-            int high = values.Count;
-            int found = BinarySearchImpl(key, values, ref low, ref high, getKeyOf);
-
-            while (low != high)
-            {
-                low = found + 1;
-                found = BinarySearchImpl(key, values, ref low, ref high, getKeyOf);
-            }
-
-            return found;
+            return SearchUpperBoundIndex(values, key, getKeyOf,
+                (KeyType a, KeyType b) => a.CompareTo(b));
         }
 
         private static int BinarySearchImpl<KeyType, DataType>(KeyType key,
                                                                IList<DataType> values,
                                                                ref int low,
                                                                ref int high,
-                                                               Func<DataType, KeyType> getKeyOf)
-            where KeyType : IComparable<KeyType>
+                                                               Func<DataType, KeyType> getKeyOf,
+                                                               Comparison<KeyType> compare)
         {
             Debug.Assert(low <= high);
 
@@ -79,11 +120,11 @@ namespace Reusable.Utils
             {
                 int middle = (low + high) / 2;
 
-                if (getKeyOf(values[middle]).CompareTo(key) < 0)
+                if (compare(getKeyOf(values[middle]), key) < 0)
                 {
                     low = middle + 1;
                 }
-                else if (getKeyOf(values[middle]).CompareTo(key) > 0)
+                else if (compare(getKeyOf(values[middle]), key) > 0)
                 {
                     high = middle;
                 }
